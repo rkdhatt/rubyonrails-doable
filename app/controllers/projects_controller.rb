2c940001c0e5f4fa1ede4b4c9f65d28af22f2616
Raman Dhatt <rkdhatt@ualberta.ca>
2025-09-14 20:44:09 -0600

    feat: Created project model without scaffolding (read and view only).

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,12 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    # projects/1
+    # params = { "id" => "1" }
+    # params[:id]
+    @project = Project.find(params[:id])
+  end
 end
