fd55d5a90f1baea73f4a0487700f2119b4075c52
Raman Dhatt <rkdhatt@ualberta.ca>
2025-09-21 11:43:42 -0600

    feat: Added CRUD operations.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -9,4 +9,46 @@ class ProjectsController < ApplicationController
     # params[:id]
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project created successfully!"
+      redirect_to project_path(@project)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update(project_params)
+      flash[:notice] = "Project updated successfully!"
+      redirect_to project_path(@project)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project deleted."
+    redirect_to projects_path
+  end
+
+  def project_params
+    # strong parameters - safety feature in Ruby on Rails
+    params.require(:project).permit(:name)
+  end
 end
